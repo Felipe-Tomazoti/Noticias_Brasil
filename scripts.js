a524842cdb0e2995bc13676c82a39e8fd994a751
Felipe Cesar Tomazoti de Souza <126637375+Felipe-Tomazoti@users.noreply.github.com>
2024-05-31 02:42:43 +0000

    att-30-05-2024

--- scripts.js
+++ scripts.js
@@ -1,8 +1,111 @@
+const valueBusca = document.getElementById('text');
+
+addEventListener("DOMContentLoaded", () => {
+    clickSvg();
+    callApi();
+});
+
+// FUNÇÕES
+
+// CHAMAR AS INFOS
+async function callData(){
+    const req = await fetch(`https://servicodados.ibge.gov.br/api/v3/noticias?qtd=5`); //${window.location.search}
+    const data = await req.json({});
+    return data;
+}
+
+// TEM QUE RETORNAR COM A URL E OS PARAMS.
+function setFilters(){
+
+}
+
+//ESTRUTURA DOS CARDS
+async function callApi() {
+    const ul = document.getElementById('ulCards')
+    const filters = setFilters();
+    //PASSAR OS "filters" como parametro do callData()
+    const data = await callData();
+    try {
+        
+        let cards = "";
+
+
+        data.items.forEach(element => {
+            
+            const titulo = element.titulo;
+            const introducao = element.introducao;
+            const editorias = '#' + element.editorias;
+            const link = element.link;
+            const img = `https://agenciadenoticias.ibge.gov.br/` + JSON.parse(!!element.imagens ? element.imagens : '{"image":{"image_intro": ""}}').image_intro;
+            const data_publicacao = converter(element.data_publicacao);
+
+            cards += `
+            <li class="liCards">
+                <img src="${img}"    
+                    alt="" class="imgClass">
+                <div class="flexColumn">
+                <div class="titleAndIntroAlignStart">
+                    <h2>${titulo}</h2>
+                    <p class="introducao">${introducao}</p>
+                </div>
+                    <div class="flex">
+                        <p class="editorias">${editorias}</p>
+                        <p class="data_publicacao">${data_publicacao}</p>
+                    </div>
+                    <button class="buttonRelease" onclick="callLink('${link}')">Leia mais</button>
+                </div>
+            </li>
+            `
+
+        });
+        ul.innerHTML=cards;
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function callLink(link){
+    window.open(link, '_blank');
+}
+
+async function filtersApplication(event){
+    event.preventDefault();
+    const newURL = new URL(window.location);
+    newURL.searchParams.set('busca', valueBusca.value);
+    const data = await callData(valueBusca.value);
+    // FAZER POR busca
+}
+
 function clickSvg() {
     const svg = document.querySelector('.svgClass');
     svg.addEventListener('click', () => {
         openDialog();
     });
+}
+
+function converter(data_publicacao){
+    const [parteData, parteHora] = data_publicacao.split(' ');
+    const [dia, mes, ano] = parteData.split('/').map(Number);
+    const [horas, minutos, segundos] = parteHora.split(':').map(Number);
+    const dataPub = new Date(ano, mes - 1, dia, horas, minutos, segundos);
+
+    const dataAtual = new Date();
+    dataAtual.setHours(0, 0, 0, 0);
+
+    const dataPubApenas = new Date(dataPub);
+    dataPubApenas.setHours(0, 0, 0, 0);
+
+    const diferencaTempo = dataAtual.getTime() - dataPubApenas.getTime();
+    const diferencaDias = Math.floor(diferencaTempo / (1000 * 60 * 60 * 24));
+
+    if (diferencaDias === 0) {
+        return 'Publicado Hoje';
+    } else if (diferencaDias === 1) {
+        return 'Publicado Ontem';
+    } else {
+        return `Publicado há ${diferencaDias} dias`;
+    }
 }
 
 function openDialog() {
@@ -13,7 +116,7 @@ function openDialog() {
     dialog.classList.add('dialogClass');
 
     dialog.innerHTML = `
-    <form class="edit-form">
+    <form id="edit-form">
         <div class="divParent">
             <div class="divParent1">
                 <div class="form-row">
@@ -63,6 +166,4 @@ function openDialog() {
     });
 }
 
-addEventListener("DOMContentLoaded", () => {
-    clickSvg();
-});
+
